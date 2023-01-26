568937e55d4307fa856fd07bdb6c934663e8ce46
Ziad Arafat <ziadarafat1@gmail.com>
2023-01-25 19:40:40 -0700

    finished printing name

--- PA001/main.c
+++ PA001/main.c
@@ -2,8 +2,29 @@
 
 int main(int argc, char const *argv[])
 {
-    int arr[50] = {0};
 
-    int *name_arr = myNameArray();
+    // cast the returned array to a char pointer and pass it to printf
+    printf((char *)my_name_array()); 
+
+
+    printf("\n"); // print a new line
+
+
+
+}
+
+/*
+Generate my name as a int array.
+Each int will store 4 chars in little endian form.
+*/
+int* my_name_array() {
+    int *name_arr = (int *)malloc(11);
+
+    name_arr[0] = 0x6461695a; // Z I A D -> D A I Z
+    name_arr[1] = 0x61724120; //   A R A -> A R A  
+    name_arr[2] = 0x746166;   // F A T   -> T A F
+    name_arr[3] = 0;          // END String
+
+    return name_arr;
 }
 
--- PA001/main.h
+++ PA001/main.h
@@ -1,13 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* myNameArray() {
-    int *name_arr = (int *)malloc(11);
-
-    name_arr[0] = 0x6461695a;
-    name_arr[1] = 0x61724120;
-    name_arr[2] = 0x746166;
-    name_arr[3] = 0;
-
-    return name_arr;
-}
+/*
+Creates an array with my name in it as ascii numbers in little endian format
+*/
+int* my_name_array();
