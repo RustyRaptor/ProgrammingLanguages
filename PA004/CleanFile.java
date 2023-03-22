17290d322324672b651b58b46c9e7ad262792c36
Ziad Arafat <ziada@nmsu.edu>
2023-03-22 07:05:03 -0600

    added java code

---
+++ PA004/CleanFile.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class CleanFile {
+    private static final String CONTROL_B = "\u0002";
+    private static final String CONTROL_C = "\u0003";
+    private static final String CARRIAGE_RET = "\n";
+    private static final String CORRUPTION_REGEX = "\\x03(?:\\x03|(?:(?![\\x0A\\x02]).)*\\x0A)*?(?:(?![\\x0A\\x02]).)*\\x02";
+
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Usage: CleanFile <input_file_path> [<output_file_path>]");
+            return;
+        }
+
+        String inputFilePath = args[0];
+        String outputFilePath = args.length > 1 ? args[1] : null;
+
+        String dirtyText;
+        try {
+            dirtyText = new String(Files.readAllBytes(Paths.get(inputFilePath)));
+        } catch (IOException e) {
+            System.out.println("Error reading input file: " + e.getMessage());
+            return;
+        }
+
+        String sanitizedText = sanitizedString(dirtyText);
+
+        if (outputFilePath != null) {
+            try {
+                Files.write(Paths.get(outputFilePath), sanitizedText.getBytes());
+            } catch (IOException e) {
+                System.out.println("Error writing output file: " + e.getMessage());
+            }
+        } else {
+            System.out.print(sanitizedText);
+        }
+    }
+
+    private static String sanitizedString(String dirtyString) {
+        Pattern pattern = Pattern.compile(CORRUPTION_REGEX, Pattern.DOTALL);
+        Matcher matcher = pattern.matcher(dirtyString);
+        return matcher.replaceAll("");
+    }
+}
